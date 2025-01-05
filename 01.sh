ccc1be9cf30af50e949ab3f324b9e7e61cf1edfb
Uday Teja <udayakaramsetty@gmail.com>
2025-01-05 13:42:14 +0530

    new file

--- 01.sh
+++ 01.sh
@@ -2,10 +2,30 @@
 
 #automating the web instances 
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE = "/tmp/$0-TIMESTAMP.log"
 
 if [ $ID  -ne  0 ]
 then 
     echo "please make it as root user"
+    exit 1
 else
     echo "root user"
 fi    
+
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then 
+    echo "$2 is success"
+    else
+}   echo "$2 is failed"
+
+
+
+dnf install nginx -y &>> LOGFILE
+VALIDATE $? "installing nginx"
+
+# systemctl enable nginx
+# systemctl start nginx
