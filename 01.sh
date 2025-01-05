8353dc6125d4122c322de9ec47b5e5c2e3aebaef
Uday Teja <udayakaramsetty@gmail.com>
2025-01-05 13:32:33 +0530

    new file

--- 01.sh
+++ 01.sh
@@ -4,9 +4,9 @@
 
 ID =$(id -u )
 echo "$ID"
-if [ $ID -ne 0 ]
-then 
-    echo "please make it as root user"
-else
-    echo "root user"
-fi    
+# if [ $ID  -ne  0 ]
+# then 
+#     echo "please make it as root user"
+# else
+#     echo "root user"
+# fi    
