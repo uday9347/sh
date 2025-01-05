9b9fb83b48840ba394ae70f3aceccd7b2f17a1f5
Uday Teja <udayakaramsetty@gmail.com>
2025-01-05 13:34:12 +0530

    new file

--- 01.sh
+++ 01.sh
@@ -2,10 +2,10 @@
 
 #automating the web instances 
 ID=$(id -u)
-echo "$ID"
-# if [ $ID  -ne  0 ]
-# then 
-#     echo "please make it as root user"
-# else
-#     echo "root user"
-# fi    
+
+if [ $ID  -ne  0 ]
+then 
+    echo "please make it as root user"
+else
+    echo "root user"
+fi    
