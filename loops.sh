c2d1d658a81a1091ca5b735ddd35eaa436a364d6
Uday Teja <udayakaramsetty@gmail.com>
2025-01-05 13:57:35 +0530

    new file

---
+++ loops.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-TIMESTAMP.log"
+echo "installing the packages"
+
+VALIDATE()
+{
+    if [$1 -ne 0 ]
+    then 
+    echo "$2 is failed"
+    else
+    echo "$2 is success"
+    fi
+}
+
+PACKAGES=("git","nginx",)
+
+for i in PACKAGES
+do 
+    dnf install i -y &>> $LOGFILE
+    VALIDATE $? "installing $i"
+done
